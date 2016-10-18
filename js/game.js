0fe4a99662d9b323d176073af0be94ed46cfdafc
aly-alina <alina.aleshkina.01@gmail.com>
2016-10-18 20:05:05 +0800

    Add start board and modify event handlers

--- js/game.js
+++ js/game.js
@@ -3,29 +3,37 @@
 var gameIsOn = false;
 var readmeIsOn = false;
 var readmeBox = document.getElementById("readme");
+var gameBoardBox = document.getElementById("gameBoard");
 
 /* -------- EVENT HANDLERS ---------- */
 
 var startGame = function(e) {
-    turnOffReadme();
-    gameIsOn = true;
+    if (!gameIsOn) {
+        if (readmeIsOn) {
+            turnOffReadme();
+        }
+        gameIsOn = true;
+        gameBoardBox.style.display = "block";
+    }
 };
 
 var stopGame = function(e) {
-    turnOffReadme();
-    gameIsOn = false;
+    if (gameIsOn) {
+        gameIsOn = false;
+        gameBoardBox.style.display = "none";
+    }
 };
 
 var showReadme = function (e) {
-    readmeIsOn = true;
-    readmeBox.style.display = "block";
+    if (!gameIsOn && !readmeIsOn) {
+        readmeIsOn = true;
+        readmeBox.style.display = "block";
+    }
 };
 
 /* -------- CONTROL ------------ */
 
 var turnOffReadme = function() {
-    if (readmeIsOn) {
-        readmeIsOn = false;
-        readmeBox.style.display = "none";
-    }
+    readmeIsOn = false;
+    readmeBox.style.display = "none";
 };
