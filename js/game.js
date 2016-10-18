9ba5797060cb9f0b4d7d66d3e99ecbab5f524ed1
aly-alina <alina.aleshkina.01@gmail.com>
2016-10-18 19:44:39 +0800

    Add readme

--- js/game.js
+++ js/game.js
@@ -1,13 +1,31 @@
 /* -------- GLOBAL VARIABLES -------- */
 
 var gameIsOn = false;
+var readmeIsOn = false;
+var readmeBox = document.getElementById("readme");
 
 /* -------- EVENT HANDLERS ---------- */
 
 var startGame = function(e) {
+    turnOffReadme();
     gameIsOn = true;
 };
 
 var stopGame = function(e) {
+    turnOffReadme();
     gameIsOn = false;
+};
+
+var showReadme = function (e) {
+    readmeIsOn = true;
+    readmeBox.style.display = "block";
+};
+
+/* -------- CONTROL ------------ */
+
+var turnOffReadme = function() {
+    if (readmeIsOn) {
+        readmeIsOn = false;
+        readmeBox.style.display = "none";
+    }
 };
