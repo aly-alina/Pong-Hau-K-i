14512597a66b90849ec7e0fb2705a9049e6e467c
aly-alina <alina.aleshkina.01@gmail.com>
2016-10-19 19:41:48 +0800

    Refactor

--- js/game.js
+++ js/game.js
@@ -6,15 +6,15 @@ var stopMessageOn = false;
 var readmeBox = document.getElementById("readme");
 var gameBoardBox = document.getElementById("gameBoard");
 var stopBox = document.getElementById("stop");
-var tokensInVertices = {
+var currentTokensPositions = {
     'topLeftVertex': "",
     'topRightVertex': "",
     'middleVertex': "",
     'bottomLeftVertex': "",
     'bottomRightVertex': ""
 };
-var initVerticesPlayer1 = ["topLeftVertex", "topRightVertex"];
-var initVerticesPlayer2 = ["bottomLeftVertex", "bottomRightVertex"];
+var initialVerticesPlayer1 = ["topLeftVertex", "topRightVertex"];
+var initialVerticesPlayer2 = ["bottomLeftVertex", "bottomRightVertex"];
 var tokens = {
     "player1": ["tokenPlayer1", "token2Player1"],
     "player2": ["tokenPlayer2", "token2Player2"]
@@ -61,8 +61,8 @@ function allowDrop(e) {
 
 function drag(e) {
     var parentId = e.target.parentElement.id;
-    if (tokensInVertices.hasOwnProperty(parentId)) {
-        tokensInVertices[parentId] = ""; // token leaves the vertex
+    if (currentTokensPositions.hasOwnProperty(parentId)) {
+        currentTokensPositions[parentId] = ""; // token leaves the vertex
     }
     e.dataTransfer.setData("Text", e.target.id);
 }
@@ -70,27 +70,28 @@ function drag(e) {
 function drop(e) {
     e.preventDefault();
     var data = e.dataTransfer.getData("Text");
-    var element = e.target;
+    var targetVertex = e.target;
 
     // prevent to add several token in the middle vertex
-    var currentChildren = element.childNodes;
-    for (var i = 0; i < currentChildren.length; i++) {
-        if (currentChildren[i].nodeName == "IMG") {
+    var currentChildrenOfVertex = targetVertex.childNodes;
+    for (var i = 0; i < currentChildrenOfVertex.length; i++) {
+        if (currentChildrenOfVertex[i].nodeName == "IMG") {
             return;
         }
     }
 
-    if (element.nodeName !== "IMG") { // prevent dragging several images in one vertex (for the rest of tokensInVertices)
-        element.appendChild(document.getElementById(data));
-        tokensInVertices[element.id] = data; // token in this vertex now
+    // prevent dragging several images in one vertex (for the rest of currentTokensPositions)
+    if (targetVertex.nodeName !== "IMG") {
+        targetVertex.appendChild(document.getElementById(data));
+        currentTokensPositions[targetVertex.id] = data; // token in this vertex now
     }
 }
 
 /* -------- CONTROL ------------ */
 
 var reset = function() {
-    initTokensOfAPlayer(initVerticesPlayer1, tokens.player1);
-    initTokensOfAPlayer(initVerticesPlayer2, tokens.player2);
+    initTokensOfAPlayer(initialVerticesPlayer1, tokens.player1);
+    initTokensOfAPlayer(initialVerticesPlayer2, tokens.player2);
 };
 
 var turnOffReadme = function() {
@@ -107,8 +108,9 @@ var turnOffStopMessage = function() {
 
 var initTokensOfAPlayer = function (initVertices, playerTokens) {
     for (var i = 0; i < initVertices.length; i++) {
+        // move token to initial position
         document.getElementById(initVertices[i])
             .appendChild(document.getElementById(playerTokens[i]));
-        tokensInVertices[initVertices[i]] = playerTokens[i];
+        currentTokensPositions[initVertices[i]] = playerTokens[i];
     }
 };
