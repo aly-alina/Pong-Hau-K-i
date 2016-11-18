65d739c9dc96c398b6e3df88dece92cc797d6695
aly-alina <alina.aleshkina.01@gmail.com>
2016-11-18 17:03:28 +0800

    Change insert statements in php

--- resources/update_user_info.php
+++ resources/update_user_info.php
@@ -5,12 +5,25 @@
             $name = $_POST['name'];
             $city = $_POST['city'];
             $age = $_POST['age'];
-            $sql_insert = "INSERT INTO users (name, city, age) 
-                           VALUES (?,?,?)";
-            $cursor = $conn->prepare($sql_insert);
-            $cursor->bindValue(1, $name);
-            $cursor->bindValue(2, $city);
-            $cursor->bindValue(3, $age);
+            $sql_insert = "INSERT INTO users (name, city, age)
+                                       VALUES (?,?,?)";
+            $sql_insert_with_latlng = "INSERT INTO users (name, city, age, lat, lng)
+                                       VALUES (?,?,?,?,?)";
+            if (isset($_POST['latitude']) && isset($_POST['longitude'])) {
+                $latitude = $_POST['latitude'];
+                $longitude = $_POST['longitude'];
+                $cursor = $conn->prepare($sql_insert_with_latlng);
+                $cursor->bindValue(1, $name);
+                $cursor->bindValue(2, $city);
+                $cursor->bindValue(3, $age);
+                $cursor->bindValue(4, $latitude);
+                $cursor->bindValue(5, $longitude);
+            } else {
+                $cursor = $conn->prepare($sql_insert);
+                $cursor->bindValue(1, $name);
+                $cursor->bindValue(2, $city);
+                $cursor->bindValue(3, $age);
+            }
             $cursor->execute();
         }
         catch(Exception $e) {
