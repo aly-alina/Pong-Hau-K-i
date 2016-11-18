6dad738bc64bb47dfffec907af117698dd8e4a1b
aly-alina <alina.aleshkina.01@gmail.com>
2016-11-18 20:46:48 +0800

    Add only users with new names to the db

--- resources/update_user_info.php
+++ resources/update_user_info.php
@@ -2,33 +2,40 @@
     include 'db_config.php';
     if(!empty($_POST)) {
         try {
-            $name = $_POST['name'];
+            $username = $_POST['name'];
             $city = $_POST['city'];
             $age = $_POST['age'];
-            $sql_insert = "INSERT INTO users (name, city, age)
+            
+            $sql_select = "SELECT * FROM users WHERE name LIKE '" . $username . "'";
+            $result = $conn->query($sql_select);
+            if ($result->rowCount() == 0) {
+                $sql_insert = "INSERT INTO users (name, city, age)
                                        VALUES (?,?,?)";
-            $sql_insert_with_latlng = "INSERT INTO users (name, city, age, lat, lng)
-                                       VALUES (?,?,?,?,?)";
-            if (isset($_POST['latitude']) && isset($_POST['longitude'])) {
-                $latitude = $_POST['latitude'];
-                $longitude = $_POST['longitude'];
-                $cursor = $conn->prepare($sql_insert_with_latlng);
-                $cursor->bindValue(1, $name);
-                $cursor->bindValue(2, $city);
-                $cursor->bindValue(3, $age);
-                $cursor->bindValue(4, $latitude);
-                $cursor->bindValue(5, $longitude);
+                $sql_insert_with_latlng = "INSERT INTO users (name, city, age, lat, lng)
+                                           VALUES (?,?,?,?,?)";
+                if (isset($_POST['latitude']) && isset($_POST['longitude'])) {
+                    $latitude = $_POST['latitude'];
+                    $longitude = $_POST['longitude'];
+                    $cursor = $conn->prepare($sql_insert_with_latlng);
+                    $cursor->bindValue(1, $username);
+                    $cursor->bindValue(2, $city);
+                    $cursor->bindValue(3, $age);
+                    $cursor->bindValue(4, $latitude);
+                    $cursor->bindValue(5, $longitude);
+                } else {
+                    $cursor = $conn->prepare($sql_insert);
+                    $cursor->bindValue(1, $username);
+                    $cursor->bindValue(2, $city);
+                    $cursor->bindValue(3, $age);
+                }
+                $cursor->execute();
+                echo "Registration was successful";
             } else {
-                $cursor = $conn->prepare($sql_insert);
-                $cursor->bindValue(1, $name);
-                $cursor->bindValue(2, $city);
-                $cursor->bindValue(3, $age);
+                echo "User is already registered";
             }
-            $cursor->execute();
         }
         catch(Exception $e) {
             die(var_dump($e));
         }
-        echo "Registration was successful";
     }
 ?>
