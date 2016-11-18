a047d46a63acc7665c4e807ee7742f9a8e4bdfca
aly-alina <alina.aleshkina.01@gmail.com>
2016-11-18 21:05:30 +0800

    Change game table

--- resources/sql_scripts/create_game_table.sql
+++ resources/sql_scripts/create_game_table.sql
@@ -1,13 +1,13 @@
 CREATE TABLE game (
     id INT NOT NULL AUTO_INCREMENT,
-    first_player_username VARCHAR(255),
+    first_player_username VARCHAR(255) NOT NULL,
     second_player_username VARCHAR(255),
     first_player_first_token VARCHAR(255),
     first_player_second_token VARCHAR(255),
     second_player_first_token VARCHAR(255),
     second_player_second_token VARCHAR(255),
     last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    game_is_on BOOLEAN,
+    game_is_on BOOLEAN DEFAULT 1,
     winner INT,
     PRIMARY KEY (id),
     FOREIGN KEY (winner) REFERENCES users(id)
